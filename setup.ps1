cb27d7c8e13025d9d3dc6b9cee9e7ea86f8400a2
Daniel Marbach <daniel.marbach@openplace.net>
2023-01-16 15:24:49 +0100

    Fix

--- setup.ps1
+++ setup.ps1
@@ -14,27 +14,9 @@ Set-Location $ScriptDirectory
 $runnerOs = $Env:RUNNER_OS ?? "Linux"
 $ravenIpsAndPortsToVerify = @{}
 
-if ($runnerOs -eq "Linux") {
-    Write-Output "Running RavenDB in container $($ContainerName) using Docker"
-
-    $powershellVersion = "lts-alpine-3.14"
-    $powerShellScriptDirectory = "/var/ravendb"
-}
-elseif ($runnerOs -eq "Windows") {
-    $powershellVersion = "lts-nanoserver-1809"
-    $powerShellScriptDirectory = "c:/ravendb"
-}
-else {
-    Write-Output "$runnerOs not supported"
-    exit 1
-}
-
 $Env:LICENSE = $RavenDBLicense
 $Env:RAVENDB_VERSION = $RavenDBVersion
 $Env:CONTAINER_NAME = $ContainerName
-$Env:POWERSHELL_VERSION = $powershellVersion
-$Env:CURRENT_DIRECTORY = "$(pwd)"
-$Env:POWERSHELL_SCRIPT_DIRECTORY = $powerShellScriptDirectory
 
 if (($RavenDBMode -eq "Single") -or ($RavenDBMode -eq "Both")) {
     docker-compose -f singlenode-compose.yml up --detach
@@ -48,14 +30,14 @@ if (($RavenDBMode -eq "Cluster") -or ($RavenDBMode -eq "Both")) {
     # Once you set the license on a node, it assumes the node to be a cluster, so only set the license on the leader
     Write-Output "Activating license on leader"
 
-    Invoke-WebRequest "http://leader:8081/admin/license/activate" -Method POST -Headers @{ 'Content-Type' = 'application/json'; 'charset' = 'UTF-8' } -Body "$($license)"
-    Invoke-WebRequest "http://leader:8081/admin/license/set-limit?nodeTag=A&newAssignedCores=1" -Method POST -Headers @{ 'Content-Type' = 'application/json'; 'Context-Length' = '0'; 'charset' = 'UTF-8' }
-    $encodedURL = [System.Web.HttpUtility]::UrlEncode("http://follower1:8082") 
-    Invoke-WebRequest "http://leader:8081/admin/cluster/node?url=$($encodedURL)&tag=B&watcher=true&assignedCores=1" -Method PUT -Headers @{ 'Content-Type' = 'application/json'; 'Context-Length' = '0'; 'charset' = 'UTF-8' }
-    $encodedURL = [System.Web.HttpUtility]::UrlEncode("http://follower2:8082)")
-    Invoke-WebRequest "http://leader:8081/admin/cluster/node?url=$($encodedURL)&tag=C&watcher=true&assignedCores=1" -Method PUT -Headers @{ 'Content-Type' = 'application/json'; 'Context-Length' = '0'; 'charset' = 'UTF-8' }
+    Invoke-WebRequest "http://leader:8080/admin/license/activate" -Method POST -Headers @{ 'Content-Type' = 'application/json'; 'charset' = 'UTF-8' } -Body "$($license)"
+    Invoke-WebRequest "http://leader:8080/admin/license/set-limit?nodeTag=A&newAssignedCores=1" -Method POST -Headers @{ 'Content-Type' = 'application/json'; 'Context-Length' = '0'; 'charset' = 'UTF-8' }
+    $encodedURL = [System.Web.HttpUtility]::UrlEncode("http://follower1:8080") 
+    Invoke-WebRequest "http://leader:8080/admin/cluster/node?url=$($encodedURL)&tag=B&watcher=true&assignedCores=1" -Method PUT -Headers @{ 'Content-Type' = 'application/json'; 'Context-Length' = '0'; 'charset' = 'UTF-8' }
+    $encodedURL = [System.Web.HttpUtility]::UrlEncode("http://follower2:8080)")
+    Invoke-WebRequest "http://leader:8080/admin/cluster/node?url=$($encodedURL)&tag=C&watcher=true&assignedCores=1" -Method PUT -Headers @{ 'Content-Type' = 'application/json'; 'Context-Length' = '0'; 'charset' = 'UTF-8' }
 }
 
 # write the connection string to the specified environment variable
 "$($SingleConnectionStringName)=http://singlenode:8080" >> $Env:GITHUB_ENV
-"$($ClusterConnectionStringName)=http://leader:8081,http://follower1:8082,http://follower2:8083" >> $Env:GITHUB_ENV
+"$($ClusterConnectionStringName)=http://leader:8080,http://follower1:8080,http://follower2:8080" >> $Env:GITHUB_ENV
