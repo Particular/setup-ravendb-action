6be243b77c703240e6d1bd1c47ba37c784be89ba
Daniel Marbach <daniel.marbach@openplace.net>
2023-01-16 11:56:52 +0100

    Try out windows container

--- cleanup.ps1
+++ cleanup.ps1
@@ -6,37 +6,13 @@ param (
 
 Set-Location $ScriptDirectory
 
-$runnerOs = $Env:RUNNER_OS ?? "Linux"
-$resourceGroup = $Env:RESOURCE_GROUP_OVERRIDE ?? "GitHubActions-RG"
-
-if ($runnerOs -eq "Linux") {
-    Write-Output "Killing Docker container $ContainerName"
-    if($RavenDBMode -eq "Single") {
-        docker-compose -f singlenode-compose.yml kill
-    }
-    if($RavenDBMode -eq "Cluster") {
-        docker-compose -f clusternodes-compose.yml kill
-    }
-
-    Write-Output "Removing Docker container $ContainerName"
-    docker rm $ContainerName
+Write-Output "Killing Docker container $ContainerName"
+if($RavenDBMode -eq "Single") {
+    docker-compose -f singlenode-compose.yml kill
 }
-elseif ($runnerOs -eq "Windows") {
-    Write-Output "Deleting Azure container(s) $ContainerName-*"
-    $containersToDelete = az container list --resource-group $resourceGroup --query "[?contains(name, '$($ContainerName)')].name" | ConvertFrom-Json
+if($RavenDBMode -eq "Cluster") {
+    docker-compose -f clusternodes-compose.yml kill
+}
 
-    foreach ($container in $containersToDelete) {
-        try {
-            Write-Output "Deleting container $container..."
-            $ignore = az container delete --name $container --resource-group $resourceGroup --yes | ConvertFrom-Json
-        }
-        catch {
-            Write-Output "Error cleaning up container"
-            Write-Output $_
-        }
-    }
-}
-else {
-    Write-Output "$runnerOs not supported"
-    exit 1
-}
+Write-Output "Removing Docker container $ContainerName"
+docker rm $ContainerName
