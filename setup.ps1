69f4d448498482adcd40b80e18efffb34c275fae
David Boike <david.boike@gmail.com>
2023-03-06 13:05:41 -0600

    Validate RavenDB license (#20)
    * Renew RavenDB license
    
    * Try failing
    
    * No really, fail
    
    * Add checks for each Invoke-WebRequest to fail the workflow
    
    * oops
    
    * Format and output details
    
    * Thanks powershell
    
    * param name
    
    * oops

--- setup.ps1
+++ setup.ps1
@@ -15,13 +15,16 @@ $runnerOs = $Env:RUNNER_OS ?? "Linux"
 $resourceGroup = $Env:RESOURCE_GROUP_OVERRIDE ?? "GitHubActions-RG"
 $ravenIpsAndPortsToVerify = @{}
 
+# Format RavenDB license as single-line
+$FormattedRavenDBLicense = ($RavenDBLicense | ConvertFrom-Json) | ConvertTo-Json -Compress
+
 if ($runnerOs -eq "Linux") {
     Write-Output "Running RavenDB in container $($ContainerName) using Docker"
 
     # This makes sure host.docker.internal is resolvable. Windows Docker adds this automatically on Linux we have to do it manually
     bash -c "echo '127.0.0.1 host.docker.internal' | sudo tee -a /etc/hosts"
 
-    $Env:LICENSE = $RavenDBLicense
+    $Env:LICENSE = $FormattedRavenDBLicense
     $Env:RAVENDB_VERSION = $RavenDBVersion
     $Env:CONTAINER_NAME = $ContainerName
 
@@ -141,22 +144,72 @@ Write-Output "::endgroup::"
 # we have to do it here. The cluster checks during the setup phase whether it can reach the nodes and that was easier to do within
 # the compose setup container. Maybe one day we will find a way to clean this up a bit.
 
+function ValidateRavenLicense {
+    param (
+        $name,
+        $hostAndPort
+    )
+    
+    Write-Output "Checking license details on $name"
+    $licenseCheck = Invoke-WebRequest "http://$($hostAndPort)/license/status" -Method GET | ConvertFrom-Json
+    if (!$?) {
+        Write-Error "Unable to check license details on $name"
+        exit -1
+    }
+
+    Write-Output "Using RavenDB License: $($licenseCheck.LicensedTo)"
+    $expDate = [datetime]::Parse($licenseCheck.Expiration)
+    Write-Output "License Expires: $($expDate.ToString("yyyy-MM-dd"))"
+    $timeLeft = $expDate - [datetime]::today
+    if ($timeLeft.Days -lt 60) {
+        Write-Output "::warning RavenDB license expires in $($timeLeft.Days) days!"
+    } else {
+        Write-Output "RavenDB license expires in $($timeLeft.Days) days"
+    }
+}
+
 if ($runnerOs -eq "Windows" -and (($RavenDBMode -eq "Single") -or ($RavenDBMode -eq "Both"))) {
     Write-Output "Activating License on Single Node"
 
-    Invoke-WebRequest "http://$($ravenIpsAndPortsToVerify['Single'].Ip):$($ravenIpsAndPortsToVerify['Single'].Port)/admin/license/activate" -Method POST -Headers @{ 'Content-Type' = 'application/json'; 'charset' = 'UTF-8' } -Body "$($RavenDBLicense)"
+    Invoke-WebRequest "http://$($ravenIpsAndPortsToVerify['Single'].Ip):$($ravenIpsAndPortsToVerify['Single'].Port)/admin/license/activate" -Method POST -Headers @{ 'Content-Type' = 'application/json'; 'charset' = 'UTF-8' } -Body "$($FormattedRavenDBLicense)"
+    if (!$?) {
+        Write-Error "Unable to activate RavenDB license on single-node server"
+        exit -1
+    }
+
+    ValidateRavenLicense "Single-Node Server" "$($ravenIpsAndPortsToVerify['Single'].Ip):$($ravenIpsAndPortsToVerify['Single'].Port)"
 }
 if ($runnerOs -eq "Windows" -and (($RavenDBMode -eq "Cluster") -or ($RavenDBMode -eq "Both"))) {
     Write-Output "Activating License on leader in the cluster"
 
     $leader = "$($ravenIpsAndPortsToVerify['Leader'].Ip):$($ravenIpsAndPortsToVerify['Leader'].Port)"
     # Once you set the license on a node, it assumes the node to be a cluster, so only set the license on the leader
-    Invoke-WebRequest "http://$($leader)/admin/license/activate" -Method POST -Headers @{ 'Content-Type' = 'application/json'; 'charset' = 'UTF-8' } -Body "$($RavenDBLicense)"
+    Invoke-WebRequest "http://$($leader)/admin/license/activate" -Method POST -Headers @{ 'Content-Type' = 'application/json'; 'charset' = 'UTF-8' } -Body "$($FormattedRavenDBLicense)"
+    if (!$?) {
+        Write-Error "Unable to activate RavenDB license on cluster leader"
+        exit -1
+    }
+
+    ValidateRavenLicense "Cluster Leader" $leader
 
     Write-Output "Establish the cluster relationship"
     Invoke-WebRequest "http://$($leader)/admin/license/set-limit?nodeTag=A&newAssignedCores=1" -Method POST -Headers @{ 'Content-Type' = 'application/json'; 'Context-Length' = '0'; 'charset' = 'UTF-8' }
+    if (!$?) {
+        Write-Error "Unable to set license limitations on cluster leader"
+        exit -1
+    }
+
     $encodedURL = [System.Web.HttpUtility]::UrlEncode("http://$($ravenIpsAndPortsToVerify['Follower1'].Ip):$($ravenIpsAndPortsToVerify['Follower1'].Port)") 
     Invoke-WebRequest "http://$($leader)/admin/cluster/node?url=$($encodedURL)&tag=B&watcher=true&assignedCores=1" -Method PUT -Headers @{ 'Content-Type' = 'application/json'; 'Context-Length' = '0'; 'charset' = 'UTF-8' }
+    if (!$?) {
+        Write-Error "Unable to join Follower1 to cluster"
+        exit -1
+    }
+
     $encodedURL = [System.Web.HttpUtility]::UrlEncode("http://$($ravenIpsAndPortsToVerify['Follower2'].Ip):$($ravenIpsAndPortsToVerify['Follower2'].Port)")
     Invoke-WebRequest "http://$($leader)/admin/cluster/node?url=$($encodedURL)&tag=C&watcher=true&assignedCores=1" -Method PUT -Headers @{ 'Content-Type' = 'application/json'; 'Context-Length' = '0'; 'charset' = 'UTF-8' }
+    if (!$?) {
+        Write-Error "Unable to join Follower 2 to cluster"
+        exit -1
+    }
 }
