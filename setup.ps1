f5e7d988216193658b8d42b27aca801cffd30629
Daniel Marbach <daniel.marbach@openplace.net>
2024-08-11 21:08:16 +0200

    Update docker compose execution to be compatible with v2

--- setup.ps1
+++ setup.ps1
@@ -39,11 +39,11 @@ if ($runnerOs -eq "Linux") {
     $Env:CONTAINER_NAME = $ContainerName
 
     if (($RavenDBMode -eq "Single") -or ($RavenDBMode -eq "Both")) {
-        docker-compose -f singlenode-compose.yml up --detach
+        docker compose -f singlenode-compose.yml up --detach
         $ravenIpsAndPortsToVerify.Add("Single", @{ Ip = "127.0.0.1"; Port = 8080 })
     }
     if (($RavenDBMode -eq "Cluster") -or ($RavenDBMode -eq "Both")) {
-        docker-compose -f clusternodes-compose.yml up --detach
+        docker compose -f clusternodes-compose.yml up --detach
         $ravenIpsAndPortsToVerify.Add("Leader", @{ Ip = "127.0.0.1"; Port = 8081 })
         $ravenIpsAndPortsToVerify.Add("Follower1", @{ Ip = "127.0.0.1"; Port = 8082 })
         $ravenIpsAndPortsToVerify.Add("Follower2", @{ Ip = "127.0.0.1"; Port = 8083 })
