87ea353669924164b6926001ff442d68e5e463f8
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-08-14 11:35:41 +0200

    Cluster URL and some cleanup

--- src/Tests/ConnectionTests.cs
+++ src/Tests/ConnectionTests.cs
@@ -9,7 +9,7 @@ public class ConnectionTests
 {
     [CancelAfter(5000)]
     [Test]
-    public void Should_establish_connection(CancellationToken cancellationToken)
+    public void Should_establish_connection_to_single_node(CancellationToken cancellationToken)
     {
         var connectionUrl = Environment.GetEnvironmentVariable("RavenDBSingleNodeUrl");
         if (connectionUrl == null)
@@ -18,15 +18,40 @@ public class ConnectionTests
             Assert.Inconclusive("No RavenDBSingleNodeUrl environment variable set");
         }
 
+        const string databaseName = "SingleNodeConnectionTests";
         var documentStore = new Raven.Client.Documents.DocumentStore
         {
             Urls = [connectionUrl],
-            Database = "ConnectionTests",
+            Database = databaseName,
         }.Initialize();
 
         Assert.DoesNotThrowAsync(async () =>
         {
-            await documentStore.Maintenance.Server.SendAsync(new CreateDatabaseOperation(new DatabaseRecord("ConnectionTests")), cancellationToken);
+            await documentStore.Maintenance.Server.SendAsync(new CreateDatabaseOperation(new DatabaseRecord(databaseName)), cancellationToken);
+        });
+    }
+
+    [CancelAfter(5000)]
+    [Test]
+    public void Should_establish_connection_to_cluster(CancellationToken cancellationToken)
+    {
+        var connectionUrl = Environment.GetEnvironmentVariable("CommaSeparatedRavenClusterUrls");
+        if (connectionUrl == null)
+        {
+            // Setting the environment variable is assumed to be checked by the CI pipeline
+            Assert.Inconclusive("No CommaSeparatedRavenClusterUrls environment variable set");
+        }
+
+        const string databaseName = "ClusterConnectionTests";
+        var documentStore = new Raven.Client.Documents.DocumentStore
+        {
+            Urls = connectionUrl.Split(','),
+            Database = databaseName,
+        }.Initialize();
+
+        Assert.DoesNotThrowAsync(async () =>
+        {
+            await documentStore.Maintenance.Server.SendAsync(new CreateDatabaseOperation(new DatabaseRecord(databaseName)), cancellationToken);
         });
     }
 }
